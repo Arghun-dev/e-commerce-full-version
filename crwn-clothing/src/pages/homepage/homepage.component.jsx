d62d7ef9a0ac8d999f7444e627873c7d4d6b42c4
Arghun-dev <arghun.developer@gmail.com>
2020-04-06 15:45:58 +0430

    Created directory-menu, menu-item components

---
+++ crwn-clothing/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//styles
+import './homepage.styles.scss'
+
+//components
+import Directory from '../../components/directory/directory.component'
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
